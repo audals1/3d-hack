be3c8ff6d804b49da17f31f00ec813ec715de56f
audals1 <97594593+audals1@users.noreply.github.com>
2024-09-08 16:55:34 +0900

    Update MagicController.cs

--- Assets/1.Main/Scripts/MagicController.cs
+++ Assets/1.Main/Scripts/MagicController.cs
@@ -29,11 +29,6 @@ public class MagicController : MonoBehaviour
             
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
