308e1f0c81e278a862488ec81ed14daf722140d7
audals1 <97594593+audals1@users.noreply.github.com>
2024-09-08 17:01:54 +0900

    Update fadeinout.cs

--- Assets/1.Main/Scripts/UI/fadeinout.cs
+++ Assets/1.Main/Scripts/UI/fadeinout.cs
@@ -7,15 +7,4 @@ public class fadeinout : MonoBehaviour
     [SerializeField]
     UISprite m_fadeBg;
     float m_time;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
